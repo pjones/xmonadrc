eb29d3dfea7cd1b5343f5a9bfde92ed5deefc848
Peter Jones <pjones@pmade.com>
2013-04-20 15:26:56 -0600

    Add a license banner to all the files for completeness

--- src/XMonad/Local/Keys.hs
+++ src/XMonad/Local/Keys.hs
@@ -1,4 +1,12 @@
 --------------------------------------------------------------------------------
+{- This file is part of the xmonadrc package. It is subject to the
+license terms in the LICENSE file found in the top-level directory of
+this distribution and at git://pmade.com/xmonadrc/LICENSE. No part of
+the xmonadrc package, including this file, may be copied, modified,
+propagated, or distributed except according to the terms contained in
+the LICENSE file. -}
+
+--------------------------------------------------------------------------------
 -- | Key bindings.
 module XMonad.Local.Keys (keys, rawKeys) where
 
--- src/XMonad/Local/Layout.hs
+++ src/XMonad/Local/Layout.hs
@@ -1,6 +1,14 @@
 {-# OPTIONS -fno-warn-missing-signatures #-}
 
 --------------------------------------------------------------------------------
+{- This file is part of the xmonadrc package. It is subject to the
+license terms in the LICENSE file found in the top-level directory of
+this distribution and at git://pmade.com/xmonadrc/LICENSE. No part of
+the xmonadrc package, including this file, may be copied, modified,
+propagated, or distributed except according to the terms contained in
+the LICENSE file. -}
+
+--------------------------------------------------------------------------------
 -- | Layout configuration and hook.
 module XMonad.Local.Layout (layoutHook) where
 
