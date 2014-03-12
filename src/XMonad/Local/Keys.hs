a12e1c1587889ac39b4aa3b5a09421963d0aa4f9
Peter Jones <pjones@pmade.com>
2014-03-12 13:42:13 -0600

    Switch from xscreensaver -lock to i3lock

--- src/XMonad/Local/Keys.hs
+++ src/XMonad/Local/Keys.hs
@@ -166,7 +166,7 @@ appKeys :: XConfig Layout -> [(String, X ())]
 appKeys c =
   [ ("C-z t",     spawn $ terminal c)
   , ("C-z C-t",   spawn "mlclient --fontsize=18")
-  , ("M-l",       spawn "xscreensaver-command -lock")
+  , ("M-l",       spawn "i3lock -dc 444444")
   , ("<Print>",   spawn "screenshot.sh root")
   , ("M-<Print>", spawn "screenshot.sh window")
   , ("M-<Space>", shellPrompt Local.promptConfig)
