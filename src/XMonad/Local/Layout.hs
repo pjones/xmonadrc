5dd575761f9a06a0d2a730fc3aa54678825e65a6
Peter Jones <pjones@pmade.com>
2015-03-10 15:49:23 -0600

    Remove unused layouts, switch default to BSP

--- src/XMonad/Local/Layout.hs
+++ src/XMonad/Local/Layout.hs
@@ -22,17 +22,15 @@ import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.Maximize
 import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.PerWorkspace (onWorkspace)
-import XMonad.Layout.Reflect (reflectHoriz)
 import XMonad.Layout.Renamed
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.SimplestFloat (simplestFloat)
-import XMonad.Layout.ThreeColumns (ThreeCol(..))
 import XMonad.Layout.TwoPane (TwoPane(..))
 
 --------------------------------------------------------------------------------
 -- | XMonad layout hook.  No type signature because it's freaking
 -- nasty and I can't come up with a way to make it generic.
-layoutHook = avoidStruts {- $ windowNavigation -} layouts
+layoutHook = avoidStruts layouts
 
 --------------------------------------------------------------------------------
 -- | All of the layouts and layout modifiers that I use.  See the
@@ -40,18 +38,14 @@ layoutHook = avoidStruts {- $ windowNavigation -} layouts
 -- signature.
 layouts =  floatF12 . maxToggle . hiddenMod $ allLays  where
   tall      = renamed [Replace "Tall"]  $ ResizableTall 1 (1.5/100) (3/5) []
-  rtall     = renamed [Replace "RTall"] $ reflectHoriz tall
-  mtall     = renamed [Replace "MTall"] $ Mirror tall
   two       = renamed [Replace "2Col"]  $ TwoPane (3/100) (3/5)
-  three     = renamed [Replace "3Col"]  $ ThreeColMid 1 (3/100) (1/2)
   full      = renamed [Replace "Full"]  $ noBorders Full
   float     = renamed [Replace "Float"] simplestFloat
   bspace    = renamed [Replace "BSP"] emptyBSP
   floatF12  = onWorkspace "F12" float
   maxToggle = renamed [CutWordsLeft 1] . maximizeWithPadding 0
   hiddenMod = renamed [CutWordsLeft 1] . hiddenWindows
-  allLays   = tall  ||| rtall ||| mtall |||
-              three ||| two   ||| full  ||| bspace
+  allLays   = bspace ||| tall ||| two ||| full
 
 --------------------------------------------------------------------------------
 -- | Use GridSelect to choose a layout.
@@ -65,10 +59,7 @@ selectLayoutByName conf = do
 
   where
     layoutNames = [ ("Tall",                   "Tall")
-                  , ("Reflected Tall",         "RTall")
-                  , ("Mirrored Tall",          "MTall")
                   , ("Two Column",             "2Col")
-                  , ("Three Column",           "3Col")
                   , ("Full Screen",            "Full")
                   , ("Full Float",             "Float")
                   , ("Binary Space Partition", "BSP")
