035f7b0d200b0fd7c564fad3cda8d45d8bc081ea
Peter Jones <pjones@pmade.com>
2015-10-21 23:16:45 -0600

    Merge commit '4d95eeb120bee17dbaf2ddf2e6c5eabd5b966897'

--- util/nix/envs/haskell-cabal.nix
+++ util/nix/envs/haskell-cabal.nix
@@ -1,14 +1,38 @@
-{ nixpkgs  ? import <nixpkgs> {}
-, compiler ? "ghc7101"
+{ nixpkgs   ? import <nixpkgs> {}
+, compiler  ? null
+, profiling ? false
 }:
 
 let
   pkgs = nixpkgs.pkgs;
-  ghc  = pkgs.haskell.packages.${compiler};
-  f    = import <pwd>;
-  drv  = ghc.callPackage f {};
+
+  # Use the default Haskell compiler if one isn't specified on the
+  # command line.
+  baseghc =
+    if compiler != null
+      then pkgs.haskell.packages.${compiler}
+      else pkgs.haskellPackages;
+
+  # Function to optionally enable profiling.
+  profilingOverride = super:
+    if profiling
+      then { mkDerivation = args: super.mkDerivation
+               (args // { enableLibraryProfiling = true; }); }
+      else { };
+
+  # Haskell compiler with overrides in place.
+  ghc = baseghc.override {
+    overrides = self: super: profilingOverride super;
+  };
+
+  # Load the generated "default.nix" from the specified directory.
+  f = import <pwd>;
+
+  # Make the actual derivation to build the Haskell project.
+  drv = ghc.callPackage f {};
 in
   (pkgs.haskell.lib.addBuildTools drv [
+    # Additional tools to make available:
     pkgs.git
     ghc.cabal-install
     ghc.hlint
