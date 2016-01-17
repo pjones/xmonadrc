041ffdca6c38e4816e87e7de064b6d7d130a0ffc
Peter Jones <pjones@pmade.com>
2016-01-16 17:39:20 -0700

    Fix broken .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((projectile-project-compilation-cmd "scripts/nix-env.sh")
-         (projectile-project-test-cmd        "dist/build/checkrc/checkrc")
-         (projectile-project-run-cmd         "scripts/install.sh restart"))))
+((nil . ((projectile-project-compilation-cmd . "scripts/nix-env.sh")
+         (projectile-project-test-cmd        . "dist/build/checkrc/checkrc")
+         (projectile-project-run-cmd         . "scripts/install.sh restart"))))
