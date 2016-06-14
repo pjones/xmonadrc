2da60d90eb6763173651915fb2b2b866c1c6adc3
Peter Jones <pjones@devalot.com>
2016-06-14 09:19:24 -0700

    Start playing with dunst

--- src/Taffybar/Local/Host.hs
+++ src/Taffybar/Local/Host.hs
@@ -41,7 +41,7 @@ hostDefault :: Host
 hostDefault =  Host
   { primaryMonitorNum = 0
   , leftWidgets       = [pager (pagerConfig 50)]
-  , rightWidgets      = [tray, clock, weather, notifications]
+  , rightWidgets      = [tray, clock, weather]
   }
 
 --------------------------------------------------------------------------------
@@ -50,5 +50,5 @@ hostHolmwood :: Host
 hostHolmwood =  Host
   { primaryMonitorNum = 0
   , leftWidgets       = [pager (pagerConfig 40)]
-  , rightWidgets      = [tray, clock, battery, weather, notifications]
+  , rightWidgets      = [tray, clock, battery, weather]
   }
