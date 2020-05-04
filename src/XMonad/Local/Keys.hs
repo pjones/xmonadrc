07a84ff0e791f66ce83ae6d6b0c9bffa312cba37
Peter Jones <pjones@devalot.com>
2020-05-04 07:53:54 -0700

    Add another scratchpad

--- src/XMonad/Local/Keys.hs
+++ src/XMonad/Local/Keys.hs
@@ -7,6 +7,7 @@ propagated, or distributed except according to the terms contained in
 the LICENSE file. -}
 
 --------------------------------------------------------------------------------
+
 -- | Key bindings.
 module XMonad.Local.Keys (keys, rawKeys) where
 
@@ -16,47 +17,44 @@ import qualified Data.Map as M
 import Graphics.X11.Xlib
 import System.Directory
 import System.FilePath ((</>))
-
 --------------------------------------------------------------------------------
 -- Package: xmonad.
 import XMonad hiding (keys)
-import qualified XMonad.StackSet as W
-
 --------------------------------------------------------------------------------
 -- Package: xmonad-contrib.
 import XMonad.Actions.CopyWindow (kill1)
-import XMonad.Actions.DynamicProjects (switchProjectPrompt, lookupProject, switchProject)
+import XMonad.Actions.DynamicProjects (switchProjectPrompt)
 import XMonad.Actions.GroupNavigation (Direction (..), nextMatch)
 import XMonad.Actions.Minimize
 import XMonad.Actions.Navigation2D
 import XMonad.Actions.PhysicalScreens (onNextNeighbour, onPrevNeighbour)
 import XMonad.Actions.Promote (promote)
-import XMonad.Actions.RotSlaves (rotSlavesUp, rotSlavesDown)
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotSlavesUp)
 import XMonad.Actions.SwapPromote (swapHybrid)
 import XMonad.Actions.TagWindows (addTag, delTag, withTagged)
 import XMonad.Actions.UpdatePointer (updatePointer)
-import XMonad.Hooks.ManageDocks (ToggleStruts(..))
+import XMonad.Hooks.ManageDocks (ToggleStruts (..))
 import XMonad.Hooks.UrgencyHook (focusUrgent)
-import XMonad.Layout.LayoutBuilder (IncLayoutN(..))
+import XMonad.Layout.LayoutBuilder (IncLayoutN (..))
 import XMonad.Layout.Maximize (maximizeRestore)
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import XMonad.Layout.ZoomRow (zoomIn, zoomOut, zoomReset)
-import XMonad.Prompt
-import XMonad.Prompt.Shell (shellPrompt)
-import XMonad.Prompt.Window (WindowPrompt(..), windowMultiPrompt, allWindows, wsWindows)
-import XMonad.Util.EZConfig (mkKeymap)
-import qualified XMonad.Util.ExtensibleState as XState
-import XMonad.Util.NamedScratchpad (namedScratchpadAction)
-
 --------------------------------------------------------------------------------
 -- Local modules.
 import XMonad.Local.Layout (selectLayoutByName, toggleLayout)
-import XMonad.Local.Layout.Columns (IncMasterCol(..))
+import XMonad.Local.Layout.Columns (IncMasterCol (..))
 import XMonad.Local.Music (radioPrompt)
 import qualified XMonad.Local.Prompt as Local
 import XMonad.Local.Tagging
-import XMonad.Local.Workspaces (asKey, viewPrevWS, scratchPads)
+import XMonad.Local.Workspaces (asKey, scratchPads, viewPrevWS)
+import XMonad.Prompt
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Prompt.Window (WindowPrompt (..), allWindows, windowMultiPrompt, wsWindows)
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig (mkKeymap)
+import qualified XMonad.Util.ExtensibleState as XState
+import XMonad.Util.NamedScratchpad (namedScratchpadAction)
 
 --------------------------------------------------------------------------------
 -- Join all the key maps into a single list and send it through @mkKeymap@.
@@ -64,20 +62,24 @@ keys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 keys c = mkKeymap c (rawKeys c)
 
 --------------------------------------------------------------------------------
+
 -- | Access the unprocessed key meant to be fed into @mkKeymap@.
 rawKeys :: XConfig Layout -> [(String, X ())]
-rawKeys c = withUpdatePointer $ concatMap ($ c) keymaps where
-  keymaps = [ baseKeys
-            , windowKeys
-            , windowTagKeys
-            , workspaceKeys
-            , layoutKeys
-            , screenKeys
-            , appKeys
-            , musicKeys
-            ]
+rawKeys c = withUpdatePointer $ concatMap ($ c) keymaps
+  where
+    keymaps =
+      [ baseKeys,
+        windowKeys,
+        windowTagKeys,
+        workspaceKeys,
+        layoutKeys,
+        screenKeys,
+        appKeys,
+        musicKeys
+      ]
 
 --------------------------------------------------------------------------------
+
 -- | Modify all keybindings so that after they finish their action the
 -- mouse pointer is moved to the corner of the focused window.  This
 -- is a bit of a hack to work around some issues I have with
@@ -92,9 +94,9 @@ withUpdatePointer = map addAction
 -- Specifically manage my prefix key (C-z), and for controlling XMonad.
 baseKeys :: XConfig Layout -> [(String, X ())]
 baseKeys c =
-  [ ("M-x r",       restartIntoDebugging)
-  , ("M-x <Space>", messageMenu c Local.promptConfig)
-  , ("M-.",         repeatLastXMessage)
+  [ ("M-x r", restartIntoDebugging),
+    ("M-x <Space>", messageMenu c Local.promptConfig),
+    ("M-.", repeatLastXMessage)
   ]
 
 --------------------------------------------------------------------------------
@@ -102,133 +104,131 @@ baseKeys c =
 windowKeys :: XConfig Layout -> [(String, X ())]
 windowKeys _ =
   -- Focusing Windows:
-  [ ("M-;",     nextMatch History (return True))
-  , ("M-C-;",   switchLayer)
-  , ("M-w k",   windows W.focusUp)
-  , ("M-w j",   windows W.focusDown)
-  , ("M-u",     focusUrgent)
-  , ("M-o",     windowPromptGoto)
-  , ("M-j",     windowGo D False)
-  , ("M-k",     windowGo U False)
-  , ("M-l",     windowGo R False)
-  , ("M-h",     windowGo L False)
-  , ("M-C-m",   windows W.focusMaster)
+  [ ("M-;", nextMatch History (return True)),
+    ("M-C-;", switchLayer),
+    ("M-w k", windows W.focusUp),
+    ("M-w j", windows W.focusDown),
+    ("M-u", focusUrgent),
+    ("M-o", windowPromptGoto),
+    ("M-j", windowGo D False),
+    ("M-k", windowGo U False),
+    ("M-l", windowGo R False),
+    ("M-h", windowGo L False),
+    ("M-C-m", windows W.focusMaster),
+    -- Moving Windows:
+    ("M-C-l", windowSwap R False),
+    ("M-C-h", windowSwap L False),
+    ("M-C-j", windowSwap D False),
+    ("M-C-k", windowSwap U False),
+    ("M-<U>", rotSlavesUp),
+    ("M-<D>", rotSlavesDown),
+    ("M-m", whenX (swapHybrid False) promote), -- Promote current window to master.
 
-  -- Moving Windows:
-  , ("M-C-l",  windowSwap R False)
-  , ("M-C-h",  windowSwap L False)
-  , ("M-C-j",  windowSwap D False)
-  , ("M-C-k",  windowSwap U False)
-  , ("M-<U>",   rotSlavesUp)
-  , ("M-<D>",   rotSlavesDown)
-  , ("M-m",     whenX (swapHybrid False) promote) -- Promote current window to master.
-
-  -- Resizing Windows:
-  , ("M-S-h",   sendMessage Shrink)
-  , ("M-S-l",   sendMessage Expand)
-  , ("M-S-j",   sendMessage MirrorShrink)
-  , ("M-S-k",   sendMessage MirrorExpand)
-
-  -- Window Layers and Killing and Yanking:
-  , ("M-w t",   withFocused $ windows . W.sink) -- Tile window.
-  , ("M-q",     kill1) -- Kill the current window.
-  , ("M-S-y",   withFocused minimizeWindow >> windows W.focusDown)
-  , ("M-S-p",   withLastMinimized maximizeWindowAndFocus)
+    -- Resizing Windows:
+    ("M-S-h", sendMessage Shrink),
+    ("M-S-l", sendMessage Expand),
+    ("M-S-j", sendMessage MirrorShrink),
+    ("M-S-k", sendMessage MirrorExpand),
+    -- Window Layers and Killing and Yanking:
+    ("M-w t", withFocused $ windows . W.sink), -- Tile window.
+    ("M-q", kill1), -- Kill the current window.
+    ("M-S-y", withFocused minimizeWindow >> windows W.focusDown),
+    ("M-S-p", withLastMinimized maximizeWindowAndFocus)
   ]
 
 --------------------------------------------------------------------------------
 -- Navigate windows by using tags.
 windowTagKeys :: XConfig Layout -> [(String, X ())]
 windowTagKeys _ =
-  [ ("M-/",   tagPrompt Local.promptConfig >> sendMessage (IncLayoutN 0))
-  , ("M-a",   primaryJumpTagUp)
-  , ("M-C-a", secondaryJumpTagUp)
-  , ("M-t a", addFocusTag)
-  , ("M-t d", rmFocusTag)
-  , ("M-t j", tagPrompt' Local.promptConfig [SetJumpTag])
-  , ("M-t r", rmFocusTagAll >> addFocusTag)
-  ] ++ numberedTags
+  [ ("M-/", tagPrompt Local.promptConfig >> sendMessage (IncLayoutN 0)),
+    ("M-a", primaryJumpTagUp),
+    ("M-C-a", secondaryJumpTagUp),
+    ("M-t a", addFocusTag),
+    ("M-t d", rmFocusTag),
+    ("M-t j", tagPrompt' Local.promptConfig [SetJumpTag]),
+    ("M-t r", rmFocusTagAll >> addFocusTag)
+  ]
+    ++ numberedTags
   where
     addFocusTag :: X ()
-    addFocusTag = do withFocused (addTag "focus")
-                     sendMessage (IncLayoutN 0)
-
+    addFocusTag = do
+      withFocused (addTag "focus")
+      sendMessage (IncLayoutN 0)
     rmFocusTag :: X ()
-    rmFocusTag = do withFocused (delTag "focus")
-                    sendMessage (IncLayoutN 0)
-
+    rmFocusTag = do
+      withFocused (delTag "focus")
+      sendMessage (IncLayoutN 0)
     rmFocusTagAll :: X ()
     rmFocusTagAll = withTagged "focus" (delTag "focus")
-
     numberedTags :: [(String, X ())]
     numberedTags = do
-      key              <- map show ([0 .. 9] :: [Int]) ++
-                          map (("F" ++) . show) ([1 .. 12] :: [Int])
+      key <-
+        map show ([0 .. 9] :: [Int])
+          ++ map (("F" ++) . show) ([1 .. 12] :: [Int])
       (prefix, action) <- numberedTemplate
       return (prefix ++ asKey key, action key)
-
     numberedTemplate :: [(String, String -> X ())]
     numberedTemplate =
-      [ ("M-",   focusTag')
-      , ("M-C-", toggleTagOnCurrentWindow)
+      [ ("M-", focusTag'),
+        ("M-C-", toggleTagOnCurrentWindow)
       ]
 
 --------------------------------------------------------------------------------
 -- Keys for manipulating workspaces.
 workspaceKeys :: XConfig Layout -> [(String, X ())]
 workspaceKeys _ =
-  [ ("M-<Space>", switchProjectPrompt Local.promptConfig)
-  , ("M-'",       viewPrevWS)
-  , ("M-f",       lookupProject "mail"     >>= mapM_ switchProject)
-  , ("M-g",       lookupProject "browsers" >>= mapM_ switchProject)
+  [ ("M-<Space>", switchProjectPrompt Local.promptConfig),
+    ("M-'", viewPrevWS)
   ]
 
 --------------------------------------------------------------------------------
 -- Layout switching and manipulation.
 layoutKeys :: XConfig Layout -> [(String, X ())]
 layoutKeys c =
-  [ ("M-<Backspace>", selectLayoutByName Local.promptConfig)
-  , ("M-w <Esc>",     setLayout (layoutHook c)) -- Reset to default layout.
-  , ("M-S-1",         withFocused (sendMessage . maximizeRestore))
-  , ("M-S-8",         toggleLayout "Focus")
-  , ("M-M1-8",        toggleLayout "Single")
-  , ("M-=",           sendMessage (IncMasterN 1))
-  , ("M--",           sendMessage (IncMasterN (-1)))
-  , ("M-S-=",         sendMessage (IncLayoutN 1))
-  , ("M-S--",         sendMessage (IncLayoutN (-1)))
-  , ("M-C-=",         sendMessage (IncMasterCol 1))
-  , ("M-C--",         sendMessage (IncMasterCol (-1)))
-  , ("M-s",           sendMessage ToggleStruts)
+  [ ("M-<Backspace>", selectLayoutByName Local.promptConfig),
+    ("M-w <Esc>", setLayout (layoutHook c)), -- Reset to default layout.
+    ("M-`", withFocused (sendMessage . maximizeRestore)),
+    ("M-f", toggleLayout "Focus"),
+    ("M-\\", toggleLayout "Single"),
+    ("M-=", sendMessage (IncMasterN 1)),
+    ("M--", sendMessage (IncMasterN (-1))),
+    ("M-S-=", sendMessage (IncLayoutN 1)),
+    ("M-S--", sendMessage (IncLayoutN (-1))),
+    ("M-C-=", sendMessage (IncMasterCol 1)),
+    ("M-C--", sendMessage (IncMasterCol (-1))),
+    ("M-s", sendMessage ToggleStruts)
   ]
 
 --------------------------------------------------------------------------------
 -- Keys to manipulate screens (actual physical monitors).
 screenKeys :: XConfig Layout -> [(String, X ())]
 screenKeys _ =
-  [ ("M-S-0", onNextNeighbour def W.view)
-  , ("M-S-9", onPrevNeighbour def W.view)
-  , ("M-\\",  screenSwap L True)
+  [ ("M-S-0", onNextNeighbour def W.view),
+    ("M-S-9", onPrevNeighbour def W.view),
+    ("M-[", screenSwap L True)
   ]
 
 --------------------------------------------------------------------------------
 -- Keys for launching applications.
 appKeys :: XConfig Layout -> [(String, X ())]
 appKeys c =
-  [ ("M-<Return>", spawn (terminal c))
-  , ("M-<Esc>",    shellPrompt Local.promptConfig)
-  , ("M-e",        emacs)
-  , ("M-p",        spawn "rofi-pass.sh")
-  , ("M-S-6",      namedScratchpadAction scratchPads "emacs")
+  [ ("M-<Return>", spawn (terminal c)),
+    ("M-<Esc>", shellPrompt Local.promptConfig),
+    ("M-e", emacs),
+    ("M-p", spawn "rofi-pass.sh"),
+    ("M-]", namedScratchpadAction scratchPads "emacs"),
+    ("M-d", namedScratchpadAction scratchPads "ffdoc")
   ]
 
 --------------------------------------------------------------------------------
 -- Keys for controlling music and volume.
 musicKeys :: XConfig Layout -> [(String, X ())]
 musicKeys _ =
-    [ ("M-r", radioPrompt Local.promptConfig)
-    ]
+  [ ("M-r", radioPrompt Local.promptConfig)
+  ]
 
 --------------------------------------------------------------------------------
+
 -- | Start an Emacs server for the current workspace.
 emacs :: X ()
 emacs = do
@@ -236,23 +236,27 @@ emacs = do
   spawn ("e -cs " ++ name)
 
 --------------------------------------------------------------------------------
+
 -- | Restart XMonad but instead of starting the XMonad in @PATH@,
 -- start a debugging version built out of my development tree.
 restartIntoDebugging :: X ()
 restartIntoDebugging = do
   home <- io getHomeDirectory
-  restart (home </> "src/rc/xmonadrc/result/bin/xmonadrc")  True
+  restart (home </> "src/rc/xmonadrc/result/bin/xmonadrc") True
 
 --------------------------------------------------------------------------------
 windowPromptGoto :: X ()
 windowPromptGoto = windowMultiPrompt Local.promptConfig modes
-  where modes = [ (Goto,      allWindows)
-                , (Goto,      wsWindows)
-                , (BringCopy, allWindows)
-                , (Bring,     allWindows)
-                ]
+  where
+    modes =
+      [ (Goto, allWindows),
+        (Goto, wsWindows),
+        (BringCopy, allWindows),
+        (Bring, allWindows)
+      ]
 
 --------------------------------------------------------------------------------
+
 -- | A menu of less frequently used actions:
 data MessageMenu = MessageMenu
 
@@ -262,43 +266,43 @@ instance XPrompt MessageMenu where
 messageMenu :: XConfig Layout -> XPConfig -> X ()
 messageMenu xc conf =
   mkXPrompt MessageMenu conf' (Local.aListCompFunc conf' actions) go
-
   where
     go :: String -> X ()
     go selected = maybe (return ()) recordXMessage $ lookup selected actions
-
     conf' :: XPConfig
-    conf' = conf { alwaysHighlight = True }
-
-    actions :: [ (String, X ()) ]
-    actions = [ ("IncLayoutN",         sendMessage (IncLayoutN 1))
-              , ("DecLayoutN",         sendMessage (IncLayoutN (-1)))
-              , ("Next Layout",        sendMessage NextLayout)
-              , ("IncMasterN",         sendMessage (IncMasterN 1))
-              , ("DecMasterN",         sendMessage (IncMasterN (-1)))
-              , ("ToggleStruts",       sendMessage ToggleStruts)
-              , ("ToggleSpacing",      toggleWindowSpacingEnabled)
-              , ("Tile Window",        withFocused $ windows . W.sink)
-              , ("Screen Spacing 0",   setScreenSpacing (Border 0 0 0 0))
-              , ("Screen Spacing +5",  incWindowSpacing 5)
-              , ("Screen Spacing -5",  decWindowSpacing 5)
-              , ("Window Spacing +5",  incWindowSpacing 5)
-              , ("Window Spacing -5",  decWindowSpacing 5)
-              , ("ZoomIn",             sendMessage zoomIn)
-              , ("ZoomOut",            sendMessage zoomOut)
-              , ("ZoomReset",          sendMessage zoomReset)
-              , ("Reset Layout",       setLayout (layoutHook xc))
-              ]
+    conf' = conf {alwaysHighlight = True}
+    actions :: [(String, X ())]
+    actions =
+      [ ("IncLayoutN", sendMessage (IncLayoutN 1)),
+        ("DecLayoutN", sendMessage (IncLayoutN (-1))),
+        ("Next Layout", sendMessage NextLayout),
+        ("IncMasterN", sendMessage (IncMasterN 1)),
+        ("DecMasterN", sendMessage (IncMasterN (-1))),
+        ("ToggleStruts", sendMessage ToggleStruts),
+        ("ToggleSpacing", toggleWindowSpacingEnabled),
+        ("Tile Window", withFocused $ windows . W.sink),
+        ("Screen Spacing 0", setScreenSpacing (Border 0 0 0 0)),
+        ("Screen Spacing +5", incWindowSpacing 5),
+        ("Screen Spacing -5", decWindowSpacing 5),
+        ("Window Spacing +5", incWindowSpacing 5),
+        ("Window Spacing -5", decWindowSpacing 5),
+        ("ZoomIn", sendMessage zoomIn),
+        ("ZoomOut", sendMessage zoomOut),
+        ("ZoomReset", sendMessage zoomReset),
+        ("Reset Layout", setLayout (layoutHook xc))
+      ]
 
 --------------------------------------------------------------------------------
+
 -- | Remember certain actions taken so they can be repeated.
 newtype LastXMessage = LastXMessage
-  { getLastMessage :: X () }
+  {getLastMessage :: X ()}
 
 instance ExtensionClass LastXMessage where
   initialValue = LastXMessage (return ())
 
 --------------------------------------------------------------------------------
+
 -- | Record the given message as the last used message, then execute it.
 recordXMessage :: X () -> X ()
 recordXMessage message = do
@@ -306,6 +310,7 @@ recordXMessage message = do
   message
 
 --------------------------------------------------------------------------------
+
 -- | Execute the last recorded message.
 repeatLastXMessage :: X ()
 repeatLastXMessage = getLastMessage =<< XState.get
--- src/XMonad/Local/Workspaces.hs
+++ src/XMonad/Local/Workspaces.hs
@@ -7,25 +7,28 @@ propagated, or distributed except according to the terms contained in
 the LICENSE file. -}
 
 --------------------------------------------------------------------------------
+
 -- | Workspace configuration and utilities.
 module XMonad.Local.Workspaces
-       ( projects
-       , terminal
-       , names
-       , scratchPads
-       , asKey
-       , viewPrevWS
-       ) where
+  ( projects,
+    terminal,
+    names,
+    scratchPads,
+    asKey,
+    viewPrevWS,
+  )
+where
 
 --------------------------------------------------------------------------------
 import Control.Monad (unless)
 import XMonad hiding (terminal)
 import XMonad.Actions.DynamicProjects
-import XMonad.Layout.LayoutCombinators (JumpToLayout(..))
+import XMonad.Layout.LayoutCombinators (JumpToLayout (..))
 import qualified XMonad.StackSet as StackSet
 import XMonad.Util.NamedScratchpad
 
 --------------------------------------------------------------------------------
+
 -- | The command to run to start a terminal.
 terminal :: String
 terminal = "konsole --notransparency --workdir $(pwd)"
@@ -33,81 +36,84 @@ terminal = "konsole --notransparency --workdir $(pwd)"
 --------------------------------------------------------------------------------
 projects :: [Project]
 projects =
-  [ Project { projectName      = "scratch"
-            , projectDirectory = "~/"
-            , projectStartHook = Just $ do
-                spawn "e -c"
-                spawn terminal
-                spawn terminal
-            }
-
-  , Project { projectName      = "browsers"
-            , projectDirectory = "~/download"
-            , projectStartHook = Just $ do
-                sendMessage (JumpToLayout "Tall")
-                spawn "firefox"
-            }
-
-  , Project { projectName      = "monitoring"
-            , projectDirectory = "~/"
-            , projectStartHook = Just $ do
-                spawn "chromium --app='https://stats.devalot.com/d/trW0cTIZz/server-health?orgId=1&refresh=10s&kiosk'"
-                spawn "chromium --app='https://stats.devalot.com/d/NeLIJZ1Zk/hq?orgId=1&from=now-1h&to=now&refresh=10s&kiosk'"
-                spawn "chromium --app='http://hass.pmade.com:8123/lovelace/0'"
-            }
-
-  , Project { projectName      = "chat"
-            , projectDirectory = "~/download"
-            , projectStartHook = Just $
-                sendMessage (JumpToLayout "Chat")
-            }
-
-  , Project { projectName      = "mail"
-            , projectDirectory = "~/"
-            , projectStartHook = Just $ do
-                sendMessage (JumpToLayout "Mail")
-                spawn "e -cs mail"
-            }
-
-  , Project { projectName      = "rc"
-            , projectDirectory = "~/src/rc"
-            , projectStartHook = Just $ do
-                spawn "e -cs rc"
-                spawn terminal
-            }
-
-  , Project { projectName      = "music"
-            , projectDirectory = "~/documents/music"
-            , projectStartHook = Just $ do
-                sendMessage (JumpToLayout "Tall")
-                spawn "spotify"
-                spawn "cantata"
-                spawn "pavucontrol"
-            }
-
-  , Project { projectName      = "rfa"
-            , projectDirectory = "~/src/rfa"
-            , projectStartHook = Just $ do
-                spawn "e -cs rfa"
-                spawn terminal
-            }
-
-  , Project { projectName      = "sthenauth"
-            , projectDirectory = "~/src/sthenauth"
-            , projectStartHook = Just $ do
-                spawn "e -cs sthenauth"
-                spawn terminal
-            }
-
-  , Project { projectName      = "iolaus"
-            , projectDirectory = "~/src/haskell/iolaus"
-            , projectStartHook = Just $ do
-                spawn "e -cs iolaus"
-                spawn terminal
-            }
+  [ Project
+      { projectName = "scratch",
+        projectDirectory = "~/",
+        projectStartHook = Just $ do
+          spawn "e -c"
+          spawn terminal
+          spawn terminal
+      },
+    Project
+      { projectName = "browsers",
+        projectDirectory = "~/download",
+        projectStartHook = Just $ do
+          sendMessage (JumpToLayout "Tall")
+          spawn "firefox"
+      },
+    Project
+      { projectName = "monitoring",
+        projectDirectory = "~/",
+        projectStartHook = Just $ do
+          spawn "chromium --app='https://stats.devalot.com/d/trW0cTIZz/server-health?orgId=1&refresh=10s&kiosk'"
+          spawn "chromium --app='https://stats.devalot.com/d/NeLIJZ1Zk/hq?orgId=1&from=now-1h&to=now&refresh=10s&kiosk'"
+          spawn "chromium --app='http://hass.pmade.com:8123/lovelace/0'"
+      },
+    Project
+      { projectName = "chat",
+        projectDirectory = "~/download",
+        projectStartHook =
+          Just $
+            sendMessage (JumpToLayout "Chat")
+      },
+    Project
+      { projectName = "mail",
+        projectDirectory = "~/",
+        projectStartHook = Just $ do
+          sendMessage (JumpToLayout "Mail")
+          spawn "e -cs mail"
+      },
+    Project
+      { projectName = "rc",
+        projectDirectory = "~/src/rc",
+        projectStartHook = Just $ do
+          spawn "e -cs rc"
+          spawn terminal
+      },
+    Project
+      { projectName = "music",
+        projectDirectory = "~/documents/music",
+        projectStartHook = Just $ do
+          sendMessage (JumpToLayout "Tall")
+          spawn "spotify"
+          spawn "cantata"
+          spawn "pavucontrol"
+      },
+    Project
+      { projectName = "rfa",
+        projectDirectory = "~/src/rfa",
+        projectStartHook = Just $ do
+          spawn "e -cs rfa"
+          spawn terminal
+      },
+    Project
+      { projectName = "sthenauth",
+        projectDirectory = "~/src/sthenauth",
+        projectStartHook = Just $ do
+          spawn "e -cs sthenauth"
+          spawn terminal
+      },
+    Project
+      { projectName = "iolaus",
+        projectDirectory = "~/src/haskell/iolaus",
+        projectStartHook = Just $ do
+          spawn "e -cs iolaus"
+          spawn terminal
+      }
   ]
 
 --------------------------------------------------------------------------------
+
 -- | Names of my workspaces.
 names :: [WorkspaceId]
 names = ["scratch", "browsers", "music", "mail", "chat"]
@@ -115,24 +121,37 @@ names = ["scratch", "browsers", "music", "mail", "chat"]
 --------------------------------------------------------------------------------
 scratchPads :: NamedScratchpads
 scratchPads =
-  [ NS { name  = "emacs"
-       , cmd   = "e -s mail -c -- -F '((name . \"scratch\"))'"
-       , query = className =? "Emacs" <&&> appName =? "scratch"
-       , hook  = floatOnRight
-       }
+  [ NS
+      { name = "emacs",
+        cmd = "e -s mail -c -- -F '((name . \"scratch\"))'",
+        query = className =? "Emacs" <&&> appName =? "scratch",
+        hook = floatOnRight
+      },
+    NS
+      { name = "ffdoc",
+        cmd = "firefox --class ffdoc --no-remote --profile ~/.mozilla/firefox/ao695ojj.Docs --kiosk https://hackage.haskell.org/",
+        query = className =? "ffdoc",
+        hook = floatOnLeft
+      }
   ]
   where
-    floatOnRight = customFloating $
-      StackSet.RationalRect (2/3) (1/10) (1/3) (8/10)
+    floatOnRight =
+      customFloating $
+        StackSet.RationalRect (2 / 3) (1 / 10) (1 / 3) (8 / 10)
+    floatOnLeft =
+      customFloating $
+        StackSet.RationalRect 0 (1 / 10) (1 / 2) (8 / 10)
 
 --------------------------------------------------------------------------------
+
 -- | Helper function to translate workspace names into key names for
 -- using in @EZConfig@ key bindings.
 asKey :: String -> String
-asKey ('F':xs) = "<F" ++ xs ++ ">" -- Function key (i.e. "F1" -> "<F1>")
-asKey x        = x                 -- Any other key.
+asKey ('F' : xs) = "<F" ++ xs ++ ">" -- Function key (i.e. "F1" -> "<F1>")
+asKey x = x -- Any other key.
 
 --------------------------------------------------------------------------------
+
 -- | Toggle between the current and previous workspaces.
 viewPrevWS :: X ()
 viewPrevWS = do
