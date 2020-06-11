60e32f0b9cb4456676b35cd41c309ba58dc923ce
Peter Jones <pjones@devalot.com>
2020-06-11 14:08:10 -0700

    Update the build system to the latest version of nix-hs

--- default.nix
+++ default.nix
@@ -1,25 +1,16 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-, ghc ? "default"
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
   cabal = ./xmonadrc.cabal;
   compiler = ghc;
 
-  overrides = lib: self: super: with lib; {
-    xmonad = super.callCabal2nix "xmonad" sources.xmonad {};
-    xmonad-contrib = super.callCabal2nix "xmonad-contrib" sources.xmonad-contrib {};
-    playlists = import sources.playlists { inherit (lib) pkgs; };
-    playlists-http = import sources.playlists-http { inherit (lib) pkgs; };
-
-    ghcide = import ghcide { ghc = compilerName; };
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
+  overrides = lib: self: super:
+    with lib; {
+      xmonad = super.callCabal2nix "xmonad" sources.xmonad { };
+      xmonad-contrib =
+        super.callCabal2nix "xmonad-contrib" sources.xmonad-contrib { };
+      playlists = import sources.playlists { inherit (lib) pkgs; };
+      playlists-http = import sources.playlists-http { inherit (lib) pkgs; };
+    };
 }
